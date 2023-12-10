e1523cdd4c3edeb17e73ecfe797ecc096af68cbd
MarcoLuw <hoanganhnk2002@gmail.com>
2023-12-10 15:04:09 +0700

    fix cors typo

--- DataWareHouse_BE/settings.py
+++ DataWareHouse_BE/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'coresheaders.middleware.CoresMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
